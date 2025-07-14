d8a861faca49085fa5b84dfeb8c9ea2d748f6024
Anton Markov <a.markov@key.dev>
2025-07-14 11:07:27 +0300

    ionic setup part2

--- apps/photo-gallery/src/pages/Tab1.tsx
+++ apps/photo-gallery/src/pages/Tab1.tsx
@@ -1,6 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { Button } from "@repo/ui/button";
 
 const Tab1: React.FC = () => {
   return (
@@ -17,6 +24,7 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <Button appName="asd" children={<div>hi from button</div>}></Button>
       </IonContent>
     </IonPage>
   );
